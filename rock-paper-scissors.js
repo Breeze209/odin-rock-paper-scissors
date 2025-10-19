367ca4ca90c30813500df90b9b8398b17e920bb9
Breeze209 <236219025+Breeze209@users.noreply.github.com>
2025-10-19 08:55:40 +0200

    Add getComputerChoice and getHumanChoice function
    The getComputerChoice function will get the choice of the
    computer using the Math.random function. The getHumanChoice function
    gets the human choice with a prompt.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+let computerChoice;
+let humanChoice;
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    computerChoice = Math.random();
+
+    if (computerChoice <= (1 / 3) && computerChoice > 0) {
+        computerChoice = 'rock';
+    } else if (computerChoice <= (2 / 3) && computerChoice > (1 / 3)) {
+        computerChoice = 'paper';
+    } else if (computerChoice <= 1 && computerChoice > (2 / 3)) {
+        computerChoice = 'scissors';
+    }
+}
+
+function getHumanChoice() {
+    humanChoice = prompt('Enter your choice:');
+}
