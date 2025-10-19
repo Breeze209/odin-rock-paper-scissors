c33a4edfdbe7780f851698700146a6173f707f50
Breeze209 <236219025+Breeze209@users.noreply.github.com>
2025-10-19 10:16:32 +0200

    Create playRound function
    The playRound function plays a round by calling the Choice functions
    and then comparing the results of those too.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,8 +13,80 @@ function getComputerChoice() {
     } else if (computerChoice <= 1 && computerChoice > (2 / 3)) {
         computerChoice = 'scissors';
     }
+    return computerChoice;
 }
 
 function getHumanChoice() {
     humanChoice = prompt('Enter your choice:');
+    return humanChoice;
 }
+
+function playRound(computerChoice, humanChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === 'rock') {
+        switch (computerChoice) {
+
+            case 'rock':
+                console.log(`It's a tie! Both players picked ${computerChoice}.`);
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+
+            case 'paper':
+                console.log(`You lose since ${computerChoice} beats ${humanChoice}.`);
+                computerScore++;
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+
+            case 'scissors':
+                console.log(`You win since ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+        }
+    } else if (humanChoice === 'paper') {
+        switch (computerChoice) {
+
+            case 'rock':
+                console.log(`You win since ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+
+            case 'paper':
+                console.log(`It's a tie! Both players picked ${computerChoice}.`);
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+
+            case 'scissors':
+                console.log(`You lose since ${computerChoice} beats ${humanChoice}.`);
+                computerScore++;
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+        }
+    } else if (humanChoice === 'scissors') {
+       switch (computerChoice) {
+
+            case 'rock':
+                console.log(`You lose since ${humanChoice} beats ${computerChoice}.`);
+                computerScore++;
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+
+            case 'paper':
+                console.log(`You win since ${computerChoice} beats ${humanChoice}.`);
+                humanScore++;
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+
+            case 'scissors':
+                console.log(`It's a tie! Both players picked ${computerChoice}.`);
+                console.log(`Your score: ${humanScore}. Computers score: ${computerScore}.`);
+                break;
+        } 
+    }
+}
+
+playRound(computerChoice, humanChoice);
